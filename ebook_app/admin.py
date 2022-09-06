5a48ad820d7003ae9aa2d4cb26258254ed6b7e29
Official0120 <niralirana2001@gmail.com>
2022-09-06 14:24:23 +0530

    updated

---
+++ ebook_app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.site_title = 'eBook House'
+admin.site.site_header = 'eBook House'
+
+my_models = (UserRole, Master, Profile, Category, Book)
+for model in my_models:
+    admin.site.register(model)
